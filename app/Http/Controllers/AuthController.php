af63f9fdfcdf6d6045ac55510403a7232417b2f8
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-15 14:57:54 +0700

    make login request & controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -18,5 +21,23 @@ class AuthController extends Controller
             'message' => 'Register successful!',
             'user' => $user
         ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials, $request->boolean('remember'))) {
+            return response()->json([
+                'message' => 'Invalid credentials!',
+            ], 401);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'message' => 'Login successful!',
+            'user' => Auth::user()
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register'])->middleware('web');
+Route::post('/login', [AuthController::class, 'login'])->middleware('web');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
