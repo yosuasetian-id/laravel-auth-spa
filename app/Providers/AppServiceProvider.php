2bce533767bbb6c942c935f3cb437a23b2e3bc8a
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-16 19:23:20 +0700

    make email logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,5 +19,25 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot(): void
+    {
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+
+            $parts = parse_url($url);
+
+            $verifyEmailUrl =
+                'http://localhost:5173/verify-email'
+                . '?id=' . $notifiable->getKey()
+                . '&hash=' . sha1($notifiable->getEmailForVerification())
+                . '&' . $parts['query'];
+
+            return (new MailMessage)
+                ->subject('Verify Email Address')
+                ->greeting('Hello ' . $notifiable->username)
+                ->line('Click the button below to verify your email address.')
+                ->action('Verify Email', $verifyEmailUrl)
+                ->line('This link will expire in 60 minutes.')
+                ->line('If you did not create an account, no action required.');
+        });
+    }
 }
