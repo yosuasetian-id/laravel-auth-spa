9ff270caeee0da0903a58a42cb343fed1d5328ac
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-16 19:19:01 +0700

    make email verification controller

---
+++ app/Http/Controllers/EmailVerificationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Verified;
+
+class EmailVerificationController extends Controller
+{
+    public function verify(Request $request, $id, $hash)
+    {
+        $user = User::findOrFail($id);
+
+        if (!hash_equals(
+            sha1($user->getEmailForVerification()),
+            $hash
+        )) {
+            abort(403, 'Invalid verification link.');
+        }
+
+        if ($user->hasVerifiedEmail()) {
+            return response()->json([
+                'message' => 'Email already verified.'
+            ]);
+        }
+
+        $user->markEmailAsVerified();
+        event(new Verified($user));
+
+        return response()->json([
+            'message' => 'Email verified successfully!'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum', 'verified');
 
 Route::post('/register', [AuthController::class, 'register'])->middleware('web');
 Route::post('/login', [AuthController::class, 'login'])->middleware('web');
